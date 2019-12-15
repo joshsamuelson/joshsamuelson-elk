03d1fae1bbf9b8a3f812ddb2375ee1df3130a611
joshsamuelson <32172285+joshsamuelson@users.noreply.github.com>
2019-12-14 21:17:35 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,4 +7,22 @@
 # @example
 #   include elk
 class elk {
+  class {'logstash':}
+  logstash::plugin {'logstash-input-beats':}
+  logstash::configfile{'beats':
+    source => 'puppet:///modules/elk/beats.conf',
+  }
+
+
+  include ::java
+  class { 'elasticsearch':
+    jvm_options => ['-Xms256m','-Xmx256m']  
+  }
+  elasticsearch::instance { 'es-01': }
+
+  class {'kibana': 
+    config => {
+      'server.host' => '0.0.0.0'
+    }
+  }
 }
